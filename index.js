979a3e2e8b48bd613effa5fe3e4016ffdde9fade
tenmark86e <tenmark86@gmail.com>
2022-08-25 12:07:28 +0300

    Done Has Target Sum Lab Solution assignment

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const seenNumbers = {};
+  for (const number of array){
+    const complement = target - number;
+    if (complement in seenNumbers) return true;
+    seenNumbers[number] = true;
 }
+return false;
+}
+
 
 /* 
   Write the Big O time complexity of your function here
